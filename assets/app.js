a8aefa87cd6c9533d77f03f76dc65015fee883b9
erdevphp <erdie2anjaratiana1998@gmail.com>
2025-02-21 22:57:25 +0300

    Ajout du logos

--- assets/app.js
+++ assets/app.js
@@ -14,7 +14,7 @@ import './js/jquery.min.js?h=89312d34339dcd686309fe284b3f226f'
 import './bootstrap/js/bootstrap.min.js?h=7c038681746a729e2fff9520a575e78c'
 import './js/chart.min.js?h=2c0fc24b3d3038317dc51c05339856d0'
 import './js/bs-init.js?h=08ba999e06fc97f895c72848bddb91c1'
-//import 'https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.js'
+import './js/jquery.easing'
 import './js/theme.js?h=6d33b44a6dcb451ae1ea7efc7b5c5e30'
 import './js/jquery.timeago'
 
